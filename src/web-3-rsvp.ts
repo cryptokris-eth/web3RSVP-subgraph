994fc6d99e374e9b5423127c3444df4772ddfd3e
cryptoKris <github@knowhere.be>
2022-08-13 17:12:01 +0200

    configure and deploy subgraph code

--- src/web-3-rsvp.ts
+++ src/web-3-rsvp.ts
@@ -1,57 +1,142 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, ipfs, json } from "@graphprotocol/graph-ts";
 import {
-  Web3RSVP,
   ConfirmedAttendee,
   DepositsPaidOut,
   NewEventCreated,
   NewRSVP
 } from "../generated/Web3RSVP/Web3RSVP"
-import { ExampleEntity } from "../generated/schema"
+import { Event, Account, RSVP, Confirmation } from "../generated/schema"
+import {integer} from "@protofire/subgraph-toolkit";
 
-export function handleConfirmedAttendee(event: ConfirmedAttendee): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleNewEventCreated(event: NewEventCreated): void {
+  // We want to make sure that we don’t create any duplicate events with the same id, 
+  //so we can try to load this event first, and if it isn’t found, we can create it and save it.
+  let newEvent = Event.load(event.params.eventID.toHex());
+  if(newEvent == null){
+    newEvent = new Event(event.params.eventID.toHex());
+    newEvent.eventID = event.params.eventID;
+    newEvent.eventOwner = event.params.creatorAddress;
+    newEvent.eventTimestamp = event.params.eventTimestamp;
+    newEvent.maxCapacity = event.params.maxCapacity;
+    newEvent.deposit = event.params.deposit;
+    newEvent.paidOut = false;
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    newEvent.totalRSVPs = integer.ZERO;
+    newEvent.totalConfirmedAttendees = integer.ZERO;
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    /**
+     * For the name, description, link, and imagePath fields, we will be using the eventCID to access data stored with ipfs 
+     * (web3.storage). 
+     * We can use the CID and the name of the event details file, data.json, to pull this data in.
+     */
+
+    let metadata = ipfs.cat(event.params.eventDataCID + "/data.json");
+
+    if (metadata) {
+      const value = json.fromBytes(metadata).toObject();
+      if (value) {
+        const name = value.get("name");
+        const description = value.get("description");
+        const link = value.get("link");
+        const imagePath = value.get("image");
+
+        if (name) {
+          newEvent.name = name.toString();
+        }
+
+        if (description) {
+          newEvent.description = description.toString();
+        }
+
+        if (link) {
+          newEvent.link = link.toString();
+        }
+
+        if (imagePath) {
+          const imageURL =
+            "https://ipfs.io/ipfs/" +
+            event.params.eventDataCID +
+            imagePath.toString();
+          newEvent.imageURL = imageURL;
+        } else {
+          // return fallback image if no imagePath
+          const fallbackURL =
+            "https://ipfs.io/ipfs/bafybeibssbrlptcefbqfh4vpw2wlmqfj2kgxt3nil4yujxbmdznau3t5wi/event.png";
+          newEvent.imageURL = fallbackURL;
+        }
+      }
+    }
+
+    //Always make sure you are saving any changes made to an entity
+    newEvent.save();
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.eventID = event.params.eventID
-  entity.attendeeAddress = event.params.attendeeAddress
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.idToEvent(...)
 }
 
-export function handleDepositsPaidOut(event: DepositsPaidOut): void {}
+function getOrCreateAccount(address: Address): Account {
+  let account = Account.load(address.toHex());
 
-export function handleNewEventCreated(event: NewEventCreated): void {}
+  if (account == null) {
+    account = new Account(address.toHex());
+    account.totalRSVPs = integer.ZERO;
+    account.totalAttendedEvents = integer.ZERO;
 
-export function handleNewRSVP(event: NewRSVP): void {}
+    account.save();
+  }
+
+  return account;
+}
+
+export function handleNewRSVP(event: NewRSVP): void {
+  let id = event.params.eventID.toHex() + event.params.attendeeAddress.toHex();
+  let newRSVP = RSVP.load(id);
+  let account = getOrCreateAccount(event.params.attendeeAddress);
+  let thisEvent = Event.load(event.params.eventID.toHex());
+
+  if (newRSVP == null && thisEvent != null) {
+    newRSVP = new RSVP(id);
+    newRSVP.attendee = account.id;
+    newRSVP.event = thisEvent.id;
+    newRSVP.save();
+    
+    thisEvent.totalRSVPs = integer.increment(thisEvent.totalRSVPs);
+    thisEvent.save();
+    
+    account.totalRSVPs = integer.increment(account.totalRSVPs);
+    account.save();
+  }
+}
+
+export function handleConfirmedAttendee(event: ConfirmedAttendee): void {
+  let id = event.params.eventID.toHex() + event.params.attendeeAddress.toHex();
+  let newConfirmation = Confirmation.load(id);
+  let account = getOrCreateAccount(event.params.attendeeAddress);
+  let thisEvent = Event.load(event.params.eventID.toHex());
+  
+  if (newConfirmation == null && thisEvent != null) {
+    newConfirmation = new Confirmation(id);
+    newConfirmation.attendee = account.id;
+    newConfirmation.event = thisEvent.id;
+    newConfirmation.save();
+
+    thisEvent.totalConfirmedAttendees = integer.increment(
+      thisEvent.totalConfirmedAttendees
+    );
+    thisEvent.save();
+
+    account.totalAttendedEvents = integer.increment(
+      account.totalAttendedEvents
+    );
+    account.save();
+  }
+}
+
+export function handleDepositsPaidOut(event: DepositsPaidOut): void {
+  let thisEvent = Event.load(event.params.eventID.toHex());
+  
+  if (thisEvent) {
+    thisEvent.paidOut = true;
+    thisEvent.save();
+  }
+}
+
+
